9654e58b3fa5a44ead045cdeb410e3a4917f2e89
Vladislav Manakov <footzi@mail.ru>
2019-08-07 22:08:17 +0300

    Add UseReduces task

--- src/UseStateHook/UseStateHook.js
+++ src/UseStateHook/UseStateHook.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 /*
     Напишите компонент с двуми инпутами и кнопкой
@@ -22,4 +22,75 @@ import React, { useState } from "react";
     https://reactjs.org/docs/hooks-reference.html#usereducer
 */
 
-export const Form = () => {};
+export const Form = () => {
+  const initialState = {
+    fields: {
+      email: "",
+      password: ""
+    },
+    isValid: false
+  };
+
+  const reducer = (state, action) => {
+    switch (action.name) {
+      case "email":
+        return {
+          ...state,
+          fields: { email: action.value, password: state.fields.password }
+        };
+      case "password":
+        return {
+          ...state,
+          fields: { email: state.fields.email, password: action.value }
+        };
+      case "validation":
+        return {
+          ...state,
+          isValid: action.value
+        };
+      default:
+        throw new Error();
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const onChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    dispatch({ name, value });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const { email, password } = state.fields;
+
+    if (email && password) {
+      dispatch({ name: "validation", value: true });
+    } else {
+      dispatch({ name: "validation", value: false });
+    }
+  };
+
+  return (
+    <form>
+      <input
+        type="email"
+        name="email"
+        data-testid="email-input"
+        value={state.fields.email}
+        onChange={onChange}
+      />
+      <input
+        type="password"
+        name="password"
+        data-testid="password-input"
+        value={state.fields.password}
+        onChange={onChange}
+      />
+      <button type="submit" data-testid="submit" onClick={onSubmit} />
+      {state.isValid && <div data-testid="success-message">Вы вошли</div>}
+    </form>
+  );
+};
